f3a327d8fc5f43ad82f0696cef4a14e6dd2533ea
Fabian P. Schmidt <kerel-fs@gmx.de>
2015-12-03 18:01:09 +0100

    manage.py: Add arguments skylines-host and -port

--- ognskylines/commands/gateway.py
+++ ognskylines/commands/gateway.py
@@ -7,7 +7,7 @@ gateway_manager = Manager()
 
 
 @gateway_manager.command
-def run(logfile=''):
+def run(skylines_host='127.0.0.1', skylines_port=5597, logfile=''):
     """Run the ogn-->skylines gateway."""
 
     # Enable logging
@@ -18,7 +18,7 @@ def run(logfile=''):
     logging.getLogger('ognskylines').setLevel('DEBUG')
 
     print('Start ogn-skylines gateway')
-    skylines_gateway = ognSkylinesGateway(session=session, aprs_user='skylines', host='127.0.0.1', port=5597)
+    skylines_gateway = ognSkylinesGateway(session=session, aprs_user='skylines', host=skylines_host, port=skylines_port)
 
     try:
         skylines_gateway.run()
