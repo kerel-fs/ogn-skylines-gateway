76d1928fdb846c526dc8a6acf97f7543d8e9cf53
Fabian P. Schmidt <kerel-fs@gmx.de>
2015-12-29 16:16:02 +0100

    database: Use a scoped session

--- ognskylines/dbutils.py
+++ ognskylines/dbutils.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine('postgresql:///ognskylines')
 
-Session = sessionmaker(bind=engine)
-session = Session()
+session = scoped_session(sessionmaker(bind=engine))
